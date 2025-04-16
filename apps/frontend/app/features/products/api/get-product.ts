b034e1e4e9020aaa35c1fe3d087d56a324bbe272
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com.>
2025-04-15 20:44:14 -0500

    add getProduct API function and integrate in edit route

---
+++ apps/frontend/app/features/products/api/get-product.ts
@@ -0,0 +1,11 @@
+import { api, type ApiResponse } from "~/lib/api-client";
+import { err, ok } from "~/lib/result";
+import type { Product } from "../types/product";
+
+export default async function getProduct(id: string) {
+    const { data } = await api.get<ApiResponse<Product>>(`/products/${id}`);
+    if (data.status === "error") {
+        return err(data.error);
+    }
+    return ok(data.data);
+}
--- apps/frontend/app/routes/products/edit.tsx
+++ apps/frontend/app/routes/products/edit.tsx
@@ -1,12 +1,27 @@
+import getProduct from "~/features/products/api/get-product";
 import ProductForm from "~/features/products/components/product-form";
+import { isErr } from "~/lib/result";
+import type { Route } from "./+types/edit";
 
-export default function EditProduct() {
+export async function clientLoader({ params }: Route.LoaderArgs) {
+  const product = await getProduct(params.productId);
+
+  if (isErr(product)) {
+    throw new Response("Error loading products", { status: 500 });
+  }
+
+  return product.value;
+}
+
+export default function EditProduct({ loaderData }: Route.ComponentProps) {
+  const product = loaderData;
+
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="max-w-2xl mx-auto">
         <h1 className="text-3xl font-bold mb-6">Edit Product</h1>
         <div className="bg-card p-6 rounded-lg shadow-sm border">
-          <ProductForm categories={[]} />
+          <ProductForm categories={[]} product={product} />
         </div>
       </div>
     </div>
